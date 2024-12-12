7fa19c9d3cbe409b54ed99ff916cd3cb9f0515d6
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-12 01:13:40 +0100

    feat: add js with all the DOM elements

--- js/script.js
+++ js/script.js
@@ -4,24 +4,53 @@ che ha visto precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da 
 indovinare sono stati individuati. */
 
-// DOM Elements 
-
-const displayElm = document.getElementById ('display');
+// DOM Elements
+const formGame = document.getElementById('form-game');
+const displayElm = document.getElementById('display');
+const colNumber = document.getElementById('col-Number');
+const btnConfirm = document.getElementById('btn-conferma');
+const btnCancel = document.getElementById('btn-annulla');
 
 // Variables
-
 let seconds = 5;
+let timerRunning = true;
 
-//MAIN
+// Funzione per generare numeri casuali tra 10 e 50
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-const idInterval = setInterval (()=>{
+// Funzione per generare e mostrare i numeri casuali negli input
+function generateNumbers() {
+    // Selezioniamo tutti gli input
+    const inputs = document.querySelectorAll('#col-Number input');
 
+    // Per ogni input, generiamo un numero casuale tra 10 e 50
+    inputs.forEach(input => {
+        const randomNumber = getRndInteger(10, 50); 
+        input.value = randomNumber; 
+    });
+
+}
+
+// Countdown timer
+const idInterval = setInterval(() => {
     if (seconds === 0) {
         clearInterval(idInterval);
-        displayElm.innerHTML = 'GO!'
+        displayElm.innerHTML = 'GO!';
+        generateNumbers(); 
+        setTimeout(() => {
+           
+            displayElm.innerHTML = '';
+            colNumber.style.display = 'block'; 
+        }, 2000);
     } else {
         displayElm.innerHTML = seconds;
-        seconds --;
+        seconds--;
     }
-
 }, 1000);
+
+// Funzione che gestisce l'evento di submit
+formGame.addEventListener('submit', function(event) {
+    event.preventDefault(); 
+});
